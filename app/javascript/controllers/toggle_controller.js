55fffa5d4304c07447ff9a9935a7ec31ea2cb417
jcampbell57 <jcampbell57@gmail.com>
2023-11-15 21:09:52 -0800

    define toggle controller targets and methods

--- app/javascript/controllers/toggle_controller.js
+++ app/javascript/controllers/toggle_controller.js
@@ -2,6 +2,46 @@ import { Controller } from "@hotwired/stimulus"
 
 // Connects to data-controller="toggle"
 export default class extends Controller {
+  static targets = [ 'input',
+                     'output',
+                     'list',
+                     'secretButton',
+                     'secretMessage',
+                     'buttonContainer']
+
   connect() {
+    console.log(`Hello from toggle controller!`)
   }
+
+  greet() {
+    this.outputTarget.textContent = `Hello, ${this.input}!`
+  }
+
+  dropdown() {
+    if (this.listTarget.hidden == true) {
+      this.listTarget.hidden = false
+    }
+    else {
+      this.listTarget.hidden = true
+    }
+  }
+
+  replace() {
+    this.secretButtonTarget.hidden = true
+    this.secretMessageTarget.hidden = false
+  }
+
+  highlight() {
+    if (this.buttonContainerTarget.style.backgroundColor == '') {
+      this.buttonContainerTarget.style.backgroundColor = "#FDFF47";
+    }
+    else {
+      this.buttonContainerTarget.style.backgroundColor = ''
+    }
+  }
+
+  get input() {
+    return this.inputTarget.value
+  }
+
 }
