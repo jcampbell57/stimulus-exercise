d7b40e343dc6f1c27339124a53be955e73788893
jcampbell57 <jcampbell57@gmail.com>
2023-11-15 21:08:53 -0800

    clean code

--- app/javascript/controllers/hello_controller.js
+++ app/javascript/controllers/hello_controller.js
@@ -2,7 +2,6 @@ import { Controller } from "@hotwired/stimulus"
 
 export default class extends Controller {
   connect() {
-    console.log(`Hello!`)
-    // this.element.textContent = "Hello World!"
+    console.log(`Hello from hello controller!`)
   }
 }
